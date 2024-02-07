22546f661052d07e6871c8b7d0a7f7e654b4ec10
Shazeena Hussain <shazeena1988@gmail.com>
2024-02-07 11:07:15 +0000

    Dinosaurs

--- session.py
+++ session.py
@@ -1,5 +1,21 @@
-print("What coat is always wet?")
-answer = input(" > ").lower()
- 
-if "paint" in answer:
-    print("Correct")
+dinosaurs = [
+    "Triceratops",
+    "Velociraptor",
+    "Anklyosaurus",
+    "Archaeopteryx",
+    "Tyrannosaurus Rex",
+    "Stegosaurus",
+    "Iguanodon"
+]
+
+saurus_dinosaurs = [
+    "Anklyosaurus",
+    "Tyrannosaurus Rex",
+    "Stegosaurus"
+]
+
+for dino in dinosaurs:
+    if "saurus" in dino:
+        saurus_dinosaurs.append(dino)
+
+print(saurus_dinosaurs)
